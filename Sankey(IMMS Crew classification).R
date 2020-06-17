b88e3c4bf8fc7e492b001569c7cf130fd9b6f6f9
AmirAli-N <AmirAli-N@users.noreply.github.com>
2020-06-17 08:14:22 -0700

    Sankey working directory, input file, and node names updated.

--- Sankey(IMMS Crew classification).R
+++ Sankey(IMMS Crew classification).R
@@ -1,25 +1,28 @@
 library(networkD3)
 library(data.table)
-setwd("C:/Users/anasr/Desktop")
+library(ggplot2)
+library(htmlwidgets)
 
-df=fread(file="Book1.csv", sep=",", header = FALSE)
-colnames(df)=c("ProposalGroups", "TableName", "ActivityCode","Description")
+setwd("Z:/PMRF/Amir")
 
-nodes=cbind.data.frame(1:(length(unique(df$ProposalGroups))+length(unique(df$TableName))), 
-                       c(unique(df$ProposalGroups), unique(df$TableName)))
+df=fread(file="group_table_act_descr.csv", sep=",", header = FALSE)
+colnames(df)=c("ProposalGroups", "TableName", "ActivityCode", "FamilyName", "Description")
+
+nodes=cbind.data.frame(1:(length(unique(df$ProposalGroups))+length(unique(df$FamilyName))), 
+                       c(unique(df$ProposalGroups), unique(df$FamilyName)))
 colnames(nodes)=c("num", "name")
 
 links=as.data.frame(matrix(NA, nrow = 0, ncol=3))
 colnames(links)=c("source","target","value")
 
 prop.group=unique(df$ProposalGroups)
-table.name=unique(df$TableName)
+table.name=unique(df$FamilyName)
 activity=unique(df$ActivityCode)
 
 for (i in prop.group){
   num_link1=0
   for (j in table.name){
-    num_link1=length(which(df$TableName==j & df$ProposalGroups==i))*10
+    num_link1=length(which(df$FamilyName==j & df$ProposalGroups==i))*10
     if (num_link1>0){
       links=rbind.data.frame(links, cbind.data.frame("source"=which(prop.group==i)-1, 
                                                      "target"=which(table.name==j)+length(prop.group)-1, 
@@ -29,9 +32,25 @@ for (i in prop.group){
 }
 p <- sankeyNetwork(Links = links, Nodes = nodes, Source = "source",
                    Target = "target", Value = "value", NodeID = "name",
-                   units = "TWh", fontSize = 14, nodeWidth = 10)
+                   units = "TWh", fontSize = 16, fontFamily = "century gothic", nodeWidth = 10)
 
 p
+
+.onRender(
+  p,
+  '
+  function(el,x){
+  // select all our node text
+  var node_text = d3.select(el)
+  .selectAll(".node text")
+  //and make them match
+  //https://github.com/christophergandrud/networkD3/blob/master/inst/htmlwidgets/sankeyNetwork.js#L180-L181
+  .attr("x", -0.5*x.options.nodeWidth)
+  .attr("text-anchor", "middle");
+  }
+  '
+)
+
 
 df=fread(file="Book2.csv", sep=",", header = FALSE)
 colnames(df)=c("TableName", "Crew")
